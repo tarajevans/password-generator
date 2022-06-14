664f0fb2fa3edc8034b052288d7520b25c083358
Tara Evans <tarajevans@hotmail.com>
2022-06-14 18:35:45 -0400

    password generator

--- Develop/script.js
+++ Develop/script.js
@@ -7,19 +7,31 @@ var lowerCase=false;
 var upperCase=false;
 var number=false;
 var symbol=false;
-var lenth;
+var length=0;
+var newPassword;
 
 //declare variable
 lowerCase=confirm("Do you want to include lower case letters? \n\n OK = Yes, Cancel = No");
 upperCase=confirm("Do you want to include upper case letters? \n\n OK = Yes, Cancel = No");
 number=confirm("Do you want to include numbers? \n\n OK = Yes, Cancel = No ");
 symbol=confirm("Do you want to include special characters? \n\n OK = Yes, Cancel = No");
+length=prompt("How many characters would you like to include? Must be between 8 and 128");
 
+//create password
+newPassword=createPassword (length);
+return newPassword;
 }
 
-
-
-
+function createPassword (pwLength) {
+var result           = "";
+var characters       = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 !\"#$%&()*+-./:;<>=?@[]^_`{}|~'\\";
+var charactersLength = characters.length;
+for ( var i = 0; i < pwLength; i++ ) {
+  result += characters.charAt(Math.floor(Math.random() * 
+charactersLength));
+}
+return result;
+}
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
