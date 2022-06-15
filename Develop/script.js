15d83187202817909f38c9826bde0b328e6674b6
Tara Evans <tarajevans@hotmail.com>
2022-06-14 20:36:16 -0400

    tested password length parameters

--- Develop/script.js
+++ Develop/script.js
@@ -3,18 +3,18 @@
 function generatePassword () {
 
 //create variable
-var lowerCase=false;
-var upperCase=false;
-var number=false;
-var symbol=false;
+var lowerCase = false;
+var upperCase = false;
+var number    = false;
+var symbol    = false;
 
-var lowerCaseChar="abcdefghijklmnopqrstuvwxyz";
-var upperCaseChar="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numberChar="0123456789";
-var symbolChar=" !\"#$%&()*+-./:;<>=?@[]^_`{}|~'\\";
+var lowerCaseChar = "abcdefghijklmnopqrstuvwxyz";
+var upperCaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numberChar    = "0123456789";
+var symbolChar    = " !\"#$%&()*+-./:;<>=?@[]^_`{}|~'\\";
 
-var includedChar="";
-var length=0;
+var includedChar = "";
+var length = 0;
 var newPassword;
 
 //declare variable
@@ -22,20 +22,24 @@ lowerCase = confirm("Do you want to include lower case letters? \n\n OK = Yes, C
 upperCase = confirm("Do you want to include upper case letters? \n\n OK = Yes, Cancel = No");
 number = confirm("Do you want to include numbers? \n\n OK = Yes, Cancel = No ");
 symbol = confirm("Do you want to include special characters? \n\n OK = Yes, Cancel = No");
-length = prompt("How many characters would you like to include? Must be between 8 and 128");
+length = getPasswordLength ();
 
 //assemble included characters
 if (lowerCase == true) {
-  includedChar += lowerCaseChar;
+  includedChar += lowerCaseChar; 
+  console.log ("lowerCase " + includedChar);
 }
 if (upperCase == true) {
   includedChar += upperCaseChar;
+  console.log ("upperCase " + includedChar);
 }
 if (number == true) {
   includedChar += numberChar;
+  console.log ("number " + includedChar);
 }
 if (symbol == true) {
   includedChar += symbolChar;
+  console.log ("symbol " + includedChar);
 }
 
 //create password
@@ -53,6 +57,22 @@ charactersLength));
 }
 return result;
 }
+
+//get and test password length
+function getPasswordLength () {
+
+//ask user for length of password
+var captureLength = prompt("How many characters would you like to include? Must be between 8 and 128");
+
+//test for correct input criteria
+while ( captureLength <8 || captureLength >128 || isNaN (captureLength)) {
+  
+  //alert to try again
+  captureLength = prompt("Attention!! Must be between number between 8 and 128");
+}
+return captureLength;
+}
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
